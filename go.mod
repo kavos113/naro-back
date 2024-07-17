e8d8d105c2b756d93a7fa6376bf283ed474059b4
kavos113 <kohei.hadano@gmail.com>
2024-07-17 09:23:37 +0900

    session

--- go.mod
+++ go.mod
@@ -1,3 +1,30 @@
 module github.com/traPtitech/naro-template-backend
 
 go 1.22.1
+
+require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/joho/godotenv v1.5.1
+	github.com/labstack/echo/v4 v4.12.0
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/gorilla/context v1.1.2 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.3.0 // indirect
+	github.com/labstack/echo-contrib v0.17.1 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/srinathgs/mysqlstore v0.0.0-20231123182912-ffbca72c0a70 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.25.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+)
---
+++ handler/handler.go
@@ -0,0 +1,187 @@
+package handler
+
+import (
+	"database/sql"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Handler struct {
+	db *sqlx.DB
+}
+
+func NewHandler(db *sqlx.DB) *Handler {
+	return &Handler{db: db}
+}
+
+type City struct {
+	ID          int            `json:"id,omitempty"  db:"ID"`
+	Name        sql.NullString `json:"name,omitempty"  db:"Name"`
+	CountryCode sql.NullString `json:"countryCode,omitempty"  db:"CountryCode"`
+	District    sql.NullString `json:"district,omitempty"  db:"District"`
+	Population  sql.NullInt64  `json:"population,omitempty"  db:"Population"`
+}
+
+func (h *Handler) GetCityInfoHandler(c echo.Context) error {
+	cityName := c.Param("cityName")
+
+	var city City
+	err := h.db.Get(&city, "SELECT * FROM city WHERE Name=?", cityName)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return c.NoContent(http.StatusNotFound)
+		}
+		log.Printf("failed to get city data: %s\n", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, city)
+}
+
+func (h *Handler) PostCityHandler(c echo.Context) error {
+	var city City
+	err := c.Bind(&city)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "bad request body")
+	}
+
+	result, err := h.db.Exec("INSERT INTO city (Name, CountryCode, District, Population) VALUES (?, ?, ?, ?)", city.Name, city.CountryCode, city.District, city.Population)
+	if err != nil {
+		log.Printf("failed to insert city data: %s\n", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		log.Printf("failed to get last insert id: %s\n", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	city.ID = int(id)
+
+	return c.JSON(http.StatusCreated, city)
+}
+
+type LoginRequestBody struct {
+	Username string `json:"username,omitempty" form:"username"`
+	Password string `json:"password,omitempty" form:"password"`
+}
+
+func (h *Handler) SignUpHandler(c echo.Context) error {
+	req := LoginRequestBody{}
+	err := c.Bind(&req)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "bad request body")
+	}
+
+	// バリデーション
+	if req.Password == "" || req.Username == "" {
+		return c.String(http.StatusBadRequest, "Username or Password is empty")
+	}
+
+	// if account exists
+	var count int
+	err = h.db.Get(&count, "SELECT COUNT(*) FROM users WHERE Username=?", req.Username)
+	if err != nil {
+		log.Println(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	if count > 0 {
+		return c.String(http.StatusConflict, "Username is already used")
+	}
+
+	// hash
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Println(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	_, err = h.db.Exec("INSERT INTO users (Username, HashedPass) VALUES (?, ?)", req.Username, hashedPass)
+	if err != nil {
+		log.Println(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.NoContent(http.StatusCreated)
+}
+
+type User struct {
+	Username   string `json:"username,omitempty" db:"Username"`
+	HashedPass string `json:"-" db:"HashedPass"`
+}
+
+func (h *Handler) LoginHandler(c echo.Context) error {
+	var req LoginRequestBody
+	err := c.Bind(&req)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "bad request body")
+	}
+
+	// バリデーション
+	if req.Password == "" || req.Username == "" {
+		return c.String(http.StatusBadRequest, "Username or Password is empty")
+	}
+
+	// fetch user from db
+	user := User{}
+	err = h.db.Get(&user, "SELECT * FROM users WHERE username=?", req.Username)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return c.NoContent(http.StatusUnauthorized)
+		} else {
+			log.Println(err)
+			return c.NoContent(http.StatusInternalServerError)
+		}
+	}
+
+	// check password
+	err = bcrypt.CompareHashAndPassword([]byte(user.HashedPass), []byte(req.Password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return c.NoContent(http.StatusUnauthorized)
+		} else {
+			return c.NoContent(http.StatusInternalServerError)
+		}
+	}
+
+	// register session
+	sess, err := session.Get("sessions", c)
+	if err != nil {
+		log.Println(err)
+		return c.String(http.StatusInternalServerError, "something wrong in getting session")
+	}
+	sess.Values["userName"] = req.Username
+	sess.Save(c.Request(), c.Response())
+
+	return c.NoContent(http.StatusOK)
+}
+
+func UserAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		sess, err := session.Get("sessions", c)
+		if err != nil {
+			log.Println(err)
+			return c.String(http.StatusInternalServerError, "something wrong in getting session")
+		}
+		if sess.Values["userName"] == nil {
+			return c.String(http.StatusUnauthorized, "please login")
+		}
+		c.Set("userName", sess.Values["userName"].(string))
+		return next(c)
+	}
+}
+
+type Me struct {
+	Username string `json:"username,omitempty" db:"username"`
+}
+
+func GetMeHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, Me{
+		Username: c.Get("username").(string),
+	})
+}
